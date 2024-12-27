cef4cc9ce50025499715d0e470bdff7d91a0b2fb
lucalocolocoloco <edijarqo@gmail.com>
2024-12-26 20:48:24 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ubah direktori ke /home/coder/project
+cd /home/coder/project
+
+# Membuat pengguna baru 'xb' tanpa password dan menambahkannya ke grup 'sudo'
+if ! id -u xb &>/dev/null; then
+    sudo adduser --disabled-password --gecos "" xb
+    sudo usermod -aG sudo xb
+fi
+
+npm init -y 
+npm install dotenv
+# Menunggu selama 4 detik untuk memastikan proses lain siap
+sleep 4
+
+# Memulai Bash terminal interaktif
+exec /bin/bash
